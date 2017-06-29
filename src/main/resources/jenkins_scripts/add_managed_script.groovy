77e077c265cda3a0417a867df59c74c7fe10ad5f
toshke <n.tosic@base2services.com>
2017-06-29 16:33:04 +1000

    WIP

---
+++ src/main/resources/jenkins_scripts/add_managed_script.groovy
@@ -0,0 +1,21 @@
+import jenkins.model.*
+import java.util.UUID.*
+import org.jenkinsci.plugins.configfiles.*
+import org.jenkinsci.lib.configprovider.model.Config;
+import org.jenkinsci.plugins.managedscripts.ScriptConfig;
+
+def inst = Jenkins.getInstance()
+
+def descriptor = 'org.jenkinsci.plugins.configfiles.GlobalConfigFiles'
+
+def desc = inst.getDescriptor(descriptor)
+
+configObj = new ScriptConfig(UUID.randomUUID().toString(),
+        "test-from-console",
+        "test-from-console-comment",
+        """#!/usr/bin/env python
+print "Hello world!!!!"
+""", null)
+
+configObj.setProviderId('org.jenkinsci.plugins.managedscripts.ScriptConfig')
+GlobalConfigFiles.get().save(configObj)
